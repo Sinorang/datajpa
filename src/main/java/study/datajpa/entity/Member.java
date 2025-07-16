2a828c466cd55c28533e90a88e3a1105c962c082
sinorang <sinorang@huray.net>
2025-07-16 15:57:10 +0900

    feat: 프로젝트 초기 설정 및 jpa 기능 학습
    - 순수 jpa 학습 위한 도메인 구현
    - 순수 jpa 사용 repository 구현
    - crud 테스트 코드 구현

---
+++ src/main/java/study/datajpa/entity/Member.java
@@ -0,0 +1,45 @@
+package study.datajpa.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(onlyExplicitlyIncluded = true)
+public class Member {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "member_id")
+    @ToString.Include
+    private Long id;
+
+    @ToString.Include
+    private String username;
+
+    @ToString.Include
+    private int age;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "team_id")
+    private Team team;
+
+    public Member(String username) {
+        this.username = username;
+    }
+
+    public Member(String username, int age, Team team) {
+        this.username = username;
+        this.age = age;
+        if (team != null) {
+            changeTeam(team);
+        }
+    }
+
+    public void changeTeam(Team team) {
+        this.team = team;
+        team.getMembers().add(this);
+    }
+}
